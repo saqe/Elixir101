dc9c0752b6675a82ae5a5071a63fd535c44d4383
Saqib <18554574+saqe@users.noreply.github.com>
2021-07-08 16:41:17 +0500

    📦 Added Sum counting from 1. to 1+2+3+..+n

--- recursion.exs
+++ recursion.exs
@@ -1,11 +1,19 @@
 defmodule Recursion do
   def factorial(n) when n in [0,1], do: n
   def factorial(n), do: n*factorial(n-1)
+
+  def sum_counting(0), do: 0
+  def sum_counting(n), do: n+sum_counting(n-1)
 end
 
+# IO.gets("Enter a number : ")
+#   |> String.trim
+#   |> String.to_integer
+#   |> Recursion.factorial
+#   |> IO.inspect
 
 IO.gets("Enter a number : ")
   |> String.trim
   |> String.to_integer
-  |> Recursion.factorial
+  |> Recursion.sum_counting
   |> IO.inspect

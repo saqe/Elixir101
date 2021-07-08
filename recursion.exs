abc68e7d74623bfa4ffc51856fabd68908bdf30d
Saqib <18554574+saqe@users.noreply.github.com>
2021-07-08 16:42:34 +0500

    📦 Added fibonaccian series with recursion

--- recursion.exs
+++ recursion.exs
@@ -4,6 +4,9 @@ defmodule Recursion do
 
   def sum_counting(0), do: 0
   def sum_counting(n), do: n+sum_counting(n-1)
+
+  def fibonaccian_series(n) when n in [0,1], do: n
+  def fibonaccian_series(n), do: fibonaccian_series(n-1) + fibonaccian_series(n-2)
 end
 
 # IO.gets("Enter a number : ")
@@ -12,8 +15,14 @@ end
 #   |> Recursion.factorial
 #   |> IO.inspect
 
+# IO.gets("Enter a number : ")
+#   |> String.trim
+#   |> String.to_integer
+#   |> Recursion.sum_counting
+#   |> IO.inspect
+
 IO.gets("Enter a number : ")
   |> String.trim
   |> String.to_integer
-  |> Recursion.sum_counting
+  |> Recursion.fibonaccian_series
   |> IO.inspect

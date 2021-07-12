be96da84b381f6027c93452853987f8848bf6746
Saqib <18554574+saqe@users.noreply.github.com>
2021-07-12 10:54:00 +0500

    👌 IMPROVED readibility

--- recursion.exs
+++ recursion.exs
@@ -1,6 +1,9 @@
 defmodule Recursion do
+  a = 3
   def factorial(n) when n in [0,1], do: n
   def factorial(n), do: n*factorial(n-1)
+
+  def get_a(), do: selfa
 
   def sum_counting(0), do: 0
   def sum_counting(n), do: n+sum_counting(n-1)
@@ -9,20 +12,24 @@ defmodule Recursion do
   def fibonaccian_series(n), do: fibonaccian_series(n-1) + fibonaccian_series(n-2)
 end
 
-# IO.gets("Enter a number : ")
-#   |> String.trim
-#   |> String.to_integer
-#   |> Recursion.factorial
-#   |> IO.inspect
 
-# IO.gets("Enter a number : ")
-#   |> String.trim
-#   |> String.to_integer
-#   |> Recursion.sum_counting
-#   |> IO.inspect
+Recursion.get_a
+  |> IO.inspect(label: "Accessing variable")
 
-IO.gets("Enter a number : ")
-  |> String.trim
-  |> String.to_integer
+number = 8
+number
+  |> Recursion.factorial
+  |> IO.inspect(label: "#{number}!")
+
+number = 8
+  |> Recursion.sum_counting
+  |> IO.inspect(label: "Sum of a list ")
+
+# This can be solve easily using the bellow code.
+number = 8
+Enum.sum(1..number)
+  |> IO.inspect(label: "Sum of a list from 1 to #{number} is ")
+
+number = 8
   |> Recursion.fibonaccian_series
-  |> IO.inspect
+  |> IO.inspect(label: "Fibonaccian Series of #{number} is ")
